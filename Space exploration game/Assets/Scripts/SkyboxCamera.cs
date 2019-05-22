7654e7c0d5e586992d3e5a3d398b99e9e044be85
Tomasz <kurek.tom@gmail.com>
2019-05-22 15:42:30 +0200

    Add SkyboxCameraTest scene

---
+++ Space exploration game/Assets/Scripts/SkyboxCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class SkyboxCamera : MonoBehaviour {
+
+    public Transform player;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        transform.rotation = player.rotation;
+	}
+}

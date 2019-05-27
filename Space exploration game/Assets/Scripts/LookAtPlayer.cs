e6d03f0d087405ea19a6719fa1fa004d8adf085e
Tomasz <kurek.tom@gmail.com>
2019-05-27 16:50:45 +0200

    Add flare object

---
+++ Space exploration game/Assets/Scripts/LookAtPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour {
+
+    private Transform playerPosition;
+
+    void Start () {
+        playerPosition = (Transform)GameObject.Find("Player").GetComponent(typeof(Transform));
+    }
+	
+	void Update () {
+        transform.LookAt(playerPosition);
+    }
+}

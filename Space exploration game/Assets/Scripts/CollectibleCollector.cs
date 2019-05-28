9767a22b115a6feed3de07068a53a3eaa33ebfd2
Tomasz <kurek.tom@gmail.com>
2019-05-28 16:45:26 +0200

    Add points text

--- Space exploration game/Assets/Scripts/CollectibleCollector.cs
+++ Space exploration game/Assets/Scripts/CollectibleCollector.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollectibleCollector : MonoBehaviour {
 
     public int points = 0;
+    public Text countText;
+
+    void Start()
+    {
+        countText.text = "Points: " + points.ToString();
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag.Equals("Collectible")) {
             points++;
+            countText.text = "Points: " + points.ToString();
             GameObject.Destroy(other.gameObject);
         }
     }

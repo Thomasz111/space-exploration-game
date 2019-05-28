fc878a15e63c3d86a27902a8d3f99980b6ed7f21
Tomasz <kurek.tom@gmail.com>
2019-05-28 18:49:08 +0200

    Add timer

---
+++ Space exploration game/Assets/Scripts/Timer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public float timeLeft = 30.0f;
+    public Text timeText;
+
+    void Update () {
+        timeLeft -= Time.deltaTime;
+        timeText.text = "Time left: " + timeLeft.ToString();
+    }
+}

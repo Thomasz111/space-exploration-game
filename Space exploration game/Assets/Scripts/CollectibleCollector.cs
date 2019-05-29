69b72fbb897c3e5762560002854250141f892636
Tomasz <kurek.tom@gmail.com>
2019-05-29 13:45:08 +0200

    Add sounds

--- Space exploration game/Assets/Scripts/CollectibleCollector.cs
+++ Space exploration game/Assets/Scripts/CollectibleCollector.cs
@@ -8,14 +8,18 @@ public class CollectibleCollector : MonoBehaviour {
     public int points = 0;
     public Text countText;
 
+    private AudioSource audio;
+
     void Start()
     {
+        audio = gameObject.GetComponent<AudioSource>();
         countText.text = "Points: " + points.ToString();
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag.Equals("Collectible")) {
+            audio.Play();
             points++;
             countText.text = "Points: " + points.ToString();
             GameObject.Destroy(other.gameObject);
